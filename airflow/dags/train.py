30feb3c337ece4aba56d3ca651c5f7bb08c92550
sashank <sashanktalakola2@gmail.com>
2024-12-12 07:55:10 -0500

    model training dags for revenuse and units model

---
+++ airflow/dags/train.py
@@ -0,0 +1,46 @@
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, year, month
+from pyspark.ml.feature import VectorAssembler, StringIndexer, OneHotEncoder
+from pyspark.ml.regression import GBTRegressor
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StandardScaler
+
+
+def main():
+    spark = SparkSession.builder \
+            .getOrCreate()
+    
+    df = spark.read.csv("/tmp/sales_data.csv", header=True, inferSchema=True)
+
+    df = df.withColumn("Order_Year", year(col("Order Date"))) \
+       .withColumn("Order_Month", month(col("Order Date")))
+    
+    categorical_columns = ["Region", "Country", "Sales Channel", "Order Priority"]
+    indexers = [StringIndexer(inputCol=col, outputCol=f"{col}_Index") for col in categorical_columns]
+    encoders = [OneHotEncoder(inputCol=f"{col}_Index", outputCol=f"{col}_Vec") for col in categorical_columns]
+
+    feature_columns = ["Order_Year", "Order_Month", "Units Sold", "Unit Price", "Unit Cost"] + \
+                  [f"{col}_Vec" for col in categorical_columns]
+    
+    train_data, test_data = df.randomSplit([0.8, 0.2], seed=42)
+
+    assembler = VectorAssembler(inputCols=feature_columns, outputCol="features")
+    scaler = StandardScaler(inputCol="features", outputCol="scaled_features")
+    gbt = GBTRegressor(featuresCol="scaled_features", labelCol="Total Revenue", predictionCol="Predicted_Revenue")
+
+    pipeline = Pipeline(stages=indexers + encoders + [assembler, scaler, gbt])
+    model = pipeline.fit(train_data)
+
+    predictions = model.transform(test_data)
+    evaluator = RegressionEvaluator(labelCol="Total Revenue", predictionCol="Predicted_Revenue", metricName="rmse")
+    rmse = evaluator.evaluate(predictions)
+    print(rmse)
+
+    model.save("/tmp/revenue_model")
+
+    spark.stop()
+
+if __name__=="__main__":
+    main()
